9b292f1eed24592a3e112617dd12a9e96d784784
mauzinn <ricioriciiorufino2008@gmail.com>
2024-03-30 11:19:22 -0300

    first commit

---
+++ file-actions/actions.mjs
@@ -0,0 +1,40 @@
+import fs from "fs"
+
+class File {
+    constructor(link, prompt, extra) {
+        this.link = link
+        this.prompt = prompt
+        this.extra = extra
+    }
+
+    Read() {
+        fs.readFileSync(this.link, this.prompt, (err, data) => {
+            if (err) {
+                return {
+                    status: false
+                }
+            }
+
+            return {
+                status: true,
+                data
+            }
+        })
+    }
+
+    Write() {
+        fs.writeFileSync(this.link, this.prompt, this.extra, (err) => {
+            if (err) {
+                return {
+                    status: false
+                }
+            }
+
+            return {
+                status: true
+            }
+        })
+    }
+}
+
+export default File
